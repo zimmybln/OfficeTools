de4753df82bea809a2bf743beab5e2aecd97b8f8
Torsten Zimmermann <zimmy@web.de>
2021-09-16 20:25:41 +0200

    Auswahl von Dateien ermöglicht

---
+++ OfficeTools/Types/WordFileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeTools.Types
+{
+    public record WordFileInfo
+    {
+        public string DisplayName { get; init; }
+
+        public string Name { get; init; }
+    }
+}
--- OfficeTools/ViewModels/CompareStylesViewModel.cs
+++ OfficeTools/ViewModels/CompareStylesViewModel.cs
@@ -1,13 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Microsoft.Win32;
+using OfficeTools.Types;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace OfficeTools.ViewModels
 {
     public class CompareStylesViewModel : BindableBase
     {
+
+
+        #region Commands
+
+        private ICommand selectFilesCommand;
+
+        public ICommand SelectFilesCommand => selectFilesCommand ??= new DelegateCommand(OnSelectFilesCommand);
+
+        private void OnSelectFilesCommand()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Dateien auswählen";
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Alle Dateien (*.*)|*.*|Word-Dokumente (*.docx)|*.docx";
+            openFileDialog.FilterIndex = 2;
+
+            if (openFileDialog.ShowDialog() != true)
+                return;
+
+            Files.Clear();
+
+            foreach (string file in openFileDialog.FileNames)
+            {
+                Files.Add(new WordFileInfo() {DisplayName = Path.GetFileName(file), Name = file});
+            }
+        }
+
+
+        #endregion
+
+
+        #region Eigenschaften
+
+        private ObservableCollection<WordFileInfo> files;
+
+        public ObservableCollection<WordFileInfo> Files
+        {
+            get => files ??= new ();
+            set => SetProperty(ref files, value);
+        }
+
+
+        #endregion
+
+
+
     }
 }
