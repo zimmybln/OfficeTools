257019e6a4feccc0c2186db97bfc8b8a7a4f1fc1
Torsten Zimmermann <zimmy@web.de>
2021-09-24 19:45:33 +0200

    Zusätzliche Analysemethoden

---
+++ OfficeTools.Test/ContentAnalyzingTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+
+namespace OfficeTools.Test
+{
+    public class ContentAnalyzingTests
+    {
+        [Test]
+        public void RetrieveParagraphText()
+        {
+            const string fileName = "Samples//DocumentWithContent.docx";
+
+            using (WordprocessingDocument output =
+                WordprocessingDocument.Open(fileName, false))
+            {
+                Document document = output.MainDocumentPart?.Document;
+
+                if (document == null)
+                    return;
+
+                Body body = document.Body;
+
+                if (body == null)
+                    return;
+
+                foreach (Paragraph paragraph in body.Descendants<Paragraph>())
+                {
+                    var innerText = paragraph.InnerText;
+
+                    if (!string.IsNullOrEmpty((innerText)))
+                    {
+                        Console.WriteLine((paragraph.InnerText));
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void RetrieveParagraphChildren()
+        {
+            const string fileName = "Samples//DocumentWithContent.docx";
+
+            using (WordprocessingDocument output =
+                WordprocessingDocument.Open(fileName, false))
+            {
+                Document document = output.MainDocumentPart?.Document;
+
+                if (document == null)
+                    return;
+
+                Body body = document.Body;
+
+                if (body == null)
+                    return;
+
+                foreach (Paragraph paragraph in body.Descendants<Paragraph>())
+                {
+                    var descendants = paragraph.Descendants().ToList();
+
+                    if (descendants.Any())
+                    {
+                        Console.WriteLine("Absatzanfang");
+                        foreach (OpenXmlElement child in paragraph.Descendants())
+                        {
+                            Console.WriteLine($"\t{child.LocalName}, {child.InnerText}");
+                        }
+
+                        Console.WriteLine("Absatzende");
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void RetrieveBoldFormattedText()
+        {
+            const string fileName = "Samples//DocumentWithContent.docx";
+
+            using (WordprocessingDocument output =
+                WordprocessingDocument.Open(fileName, false))
+            {
+                Document document = output.MainDocumentPart?.Document;
+
+                if (document == null)
+                    return;
+
+                Body body = document.Body;
+
+                if (body == null)
+                    return;
+
+                foreach (Run run in body.Descendants<Run>())
+                {
+                    // <Run><RunProperties><Bold>...
+
+                    string runDescriptor = String.Empty;
+
+                    RunProperties runProperties = run.Descendants<RunProperties>().FirstOrDefault();
+
+                    if (runProperties == null)
+                        continue;
+                    
+                    if (runProperties.Bold != null && runProperties.Bold.Val?.Value != false)
+                    {
+                        Console.WriteLine($"{run.InnerText}");    
+                    }
+                }
+            }
+        }
+
+        public void RetrieveParagraphStyleProperties()
+        {
+
+        }
+    }
+}
--- OfficeTools.Test/ContentCreationTests.cs
+++ OfficeTools.Test/ContentCreationTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using NUnit.Framework;
@@ -12,22 +13,61 @@ using OpenXmlPowerTools;
 
 namespace OfficeTools.Test
 {
-    class FileProcessingTests
+    class ContentCreationTests
     {
         [SetUp]
         public void Setup()
         {
-            if (!Directory.Exists("Files"))
+            if (!Directory.Exists("Samples"))
             {
-                Directory.CreateDirectory("Files");
+                Directory.CreateDirectory("Samples");
             }
         }
-
 
         [Test]
         public void CreateDocumentWithContent()
         {
             const string fileName = "Samples//created_content.docx";
+
+            try
+            {
+                if (File.Exists(fileName))
+                    File.Delete(fileName);
+
+                
+                using (WordprocessingDocument output = WordprocessingDocument.Create(fileName, WordprocessingDocumentType.Document))
+                {
+                    output.AddMainDocumentPart();
+
+                    var text = new Text($"Test {MethodBase.GetCurrentMethod().Name} {DateTime.Now.ToString()}");
+
+                    var run = new Run(text);
+
+                    var paragraph = new Paragraph(run);
+
+                    var body = new Body(paragraph);
+
+                    output.MainDocumentPart.Document = new Document(body);
+
+                    output.Save();
+                }
+
+
+                Assert.IsTrue(File.Exists(fileName));
+            }
+            finally
+            {
+
+            }
+
+
+        }
+
+
+        [Test]
+        public void CreateDocumentWithContent_PT()
+        {
+            const string fileName = "Samples//created_content_pt.docx";
 
             try
             {
