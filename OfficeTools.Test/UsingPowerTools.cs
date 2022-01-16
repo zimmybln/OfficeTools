6e7b0968309631e69960904fc2db09897b6b4023
Torsten Zimmermann <zimmy@web.de>
2022-01-16 12:08:45 +0100

    Dokumente kombinieren

--- OfficeTools.Test/UsingPowerTools.cs
+++ OfficeTools.Test/UsingPowerTools.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using OpenXmlPowerTools;
 
 namespace OfficeTools.Test
 {
@@ -13,7 +15,19 @@ namespace OfficeTools.Test
         [Test]
         public void CombineDocuments()
         {
-             
+            string firstDocument = "Samples//CombineDocument1.docx";
+            string secondDocument = "Samples//CombineDocument2.docx";
+
+            string targetDocument = "Samples//CombinedDocument.docx";
+
+            List<Source> sourceDocuments = new ()
+            {
+                new Source(new WmlDocument(firstDocument)),
+                new Source(new WmlDocument(secondDocument))
+            };
+
+            DocumentBuilder.BuildDocument(sourceDocuments, targetDocument);
+
         }
     }
 }
